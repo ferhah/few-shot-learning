6a792e482405a08ea161f5e8fdb7b28094c20694
Ferdinand Hahmann <git@fhahmann.de>
2020-03-16 08:25:47 +0100

    Extract method for finetuning and evaluate finetuned model in one method

--- evaluation/main.py
+++ evaluation/main.py
@@ -43,6 +43,19 @@ def evaluate_model(dataloader, model, device):
     return predictions, labels
 
 
+def evaluate(evaluationdir, approach, train_batch_size, val_batch_size, num_workers, log_dir, test_name, datadir,
+             device):
+    dataloader_train = get_dataloader(os.path.join(evaluationdir, 'train'), datadir, approach.train_transforms,
+                                      train_batch_size, num_workers, True)
+    dataloader_test= get_dataloader(os.path.join(evaluationdir, test_name), datadir, approach.inference_transforms,
+                                    val_batch_size, num_workers, False)
+    model, train_kpis = finetune_model(dataloader_train, approach, log_dir)
+    predictions, labels = evaluate_model(dataloader_test, model, device)
+    # Calculate KPIs
+    results = calculate_kpis(predictions, labels)
+    return results, train_kpis
+
+
 def evaluation(testdir, outfilename, approaches, device, train_batch_size, val_batch_size, num_workers, datadir=None):
     # List of all dirs, containing a small training dataset (/train or /train.txt) and a  test dir (/test or /test.txt).
     evaluationsdirs = [os.path.join(testdir, x) for x in os.listdir(testdir)]
