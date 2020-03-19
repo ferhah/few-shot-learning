3e8d9be17542df7ab7e22802031f6dc954bafeef
Ferdinand Hahmann <git@fhahmann.de>
2020-03-19 17:41:36 +0100

    Adding module for loading pretrained MAML model

---
+++ evaluation/maml_pretrained.py
@@ -0,0 +1,17 @@
+import torch
+from maml.model import ModelConvMiniImagenet
+import torch.nn
+import torch.nn.functional as F
+
+class MAML():
+    def __init__(self, modelpath=None, device=None):
+        if device is None:
+            device = 'cpu'
+        self.device = device
+        self.modelpath = modelpath
+        self.model = ModelConvMiniImagenet(10, hidden_size=64)
+        if modelpath:
+            self.model.load_state_dict(torch.load(self.modelpath))
+        self.num_adaptation_steps=1
+        self.loss_function = F.cross_entropy
+
